356a45e730e05583f5054f0e0a1ac637073ba5fb
RomanZhukov-b <zukovroman232@gmail.com>
2023-08-27 14:57:04 +0300

    add toy_store

---
+++ MyToyStore/Client/Program.java
@@ -0,0 +1,9 @@
+import MyToyStore.Core.UI.App;
+
+public class Program {
+
+    public static void main(String[] args) {
+        App.startButtonClick();
+    }
+    
+}
---
+++ MyToyStore/Core/MVP/View.java
@@ -0,0 +1,9 @@
+public interface View {
+
+    String getToyName();
+
+    int getToyProbability();
+
+    void display(String data);
+
+}
